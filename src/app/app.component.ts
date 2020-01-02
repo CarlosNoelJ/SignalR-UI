8e889f8bf12f41e72232876cdb9a9759d918df03
Carlos Noel <Carlos.Noel@skygenusa.com>
2020-01-02 07:27:51 -0600

    signalR-UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import * as signalR from '@aspnet/signalr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [MessageService]
 })
-export class AppComponent {
-  title = 'client-notification';
+export class AppComponent implements OnInit{
+
+  constructor(private messageService: MessageService) {}
+
+  ngOnInit(): void {
+    const connection = new signalR.HubConnectionBuilder()
+    .configureLogging(signalR.LogLevel.Information)
+    .withUrl('http://localhost:5000/notify')
+    .build();
+
+    connection.start().then( () => {
+      console.log('connected!');
+    }).catch((err) => {
+      return console.error(err.toString());
+    });
+
+    // tslint:disable-next-line: no-shadowed-variable
+    connection.on('BroadcastMessage', ( type: string, payload: string) => {
+      this.messageService.add({ severity: type, summary: payload, detail: 'Vía SignalR'});
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { ToastModule} from 'primeng/toast';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +9,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ToastModule
   ],
   providers: [],
   bootstrap: [AppComponent]
